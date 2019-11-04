01ff89faaf40cec8d69c72789582dad5479d5810
10604308 <Yishan_Chen@wistron.com>
2019-11-04 17:57:08 +0800

    load env config

--- backend/api/postgres.js
+++ backend/api/postgres.js
@@ -4,14 +4,16 @@ const { Pool } = require('pg');
 const router = new Router();
 
 // const { db } = require('../lib/database')
-
-const config =  {
+var config = {};
+if (process.env.NODE_ENV === 'production') {
+  const config =  {
   host: 'ec2-23-21-177-102.compute-1.amazonaws.com',
   database: 'df0qaq6p88urtq',
   user: 'knaoglylddnsjq',
   password: '5470b3d6357c03d1c1fcf8af6d3914543beb360c8a179c2f794dfa5a2b46d3ab',
   port: 5432,
   ssl: true
+  }
 }
 
 const pool = new Pool(config);
